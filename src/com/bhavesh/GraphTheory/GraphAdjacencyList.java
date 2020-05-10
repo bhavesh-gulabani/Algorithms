7c8481b522119f4ac549aee0d5f926e77f700d8f
bhavesh-gulabani <bhaveshgulabani1@gmail.com>
2020-05-10 18:38:30 +0530

    Added adjacency list representation

---
+++ src/com/bhavesh/GraphTheory/GraphAdjacencyList.java
@@ -0,0 +1,47 @@
+/*
+ * Implementation of an adjacency list representation of a graph
+ */
+
+package com.bhavesh.GraphTheory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GraphAdjacencyList {
+    Map<Integer, List<Edge>> graph = new HashMap<>();
+    int numNodes;
+
+    static class Edge {
+        int from, to, cost;
+
+        public Edge(int from, int to, int cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
+        }
+    }
+
+    public GraphAdjacencyList(int numNodes) {
+        this.numNodes = numNodes;
+    }
+
+    public int getNumNodes() {
+        return numNodes;
+    }
+
+    public List<Edge> getListOfEdges(int at) {
+        return graph.get(at);
+    }
+
+    public void addDirectedEdge(int from, int to, int cost) {
+        List<Edge> list = graph.get(from);
+        if (list == null) {
+            list = new ArrayList<Edge>();
+            graph.put(from, list);
+        }
+        list.add(new Edge(from, to, cost));
+    }
+}
+
